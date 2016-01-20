7e621ea3a56f11e58eeb1469c23ae49e1921bc35
Sergey Sadovoi <serg.sadovoi@gmail.com>
2016-01-20 12:15:54 +0200

    Variables rule

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
     extends: [
         'hope/rules/errors',
         'hope/rules/best-practices',
-        'hope/rules/strict'
+        'hope/rules/strict',
+        'hope/rules/variables'
     ],
     rules: {}
 };
