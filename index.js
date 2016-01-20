aa64a3cfe942c51dc482dbec7ccab73fb6c67892
Sergey Sadovoi <serg.sadovoi@gmail.com>
2016-01-20 12:09:57 +0200

    Strict rule

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = {
     extends: [
         'hope/rules/errors',
-        'hope/rules/best-practices'
+        'hope/rules/best-practices',
+        'hope/rules/strict'
     ],
     rules: {}
 };
