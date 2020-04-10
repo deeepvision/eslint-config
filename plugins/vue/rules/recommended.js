92e2439f406a977d13622e2499cb37f56fc2cb68
Tanya <angolenkotanya@gmail.com>
2020-04-10 16:39:52 +0300

    fix: rules

--- plugins/vue/rules/recommended.js
+++ plugins/vue/rules/recommended.js
@@ -1,6 +1,21 @@
 module.exports = {
     rules: {
-        'vue/attributes-order': 'error',
+        'vue/attributes-order': ['error', {
+            'order': [
+                'DEFINITION',
+                'LIST_RENDERING',
+                'CONDITIONALS',
+                'RENDER_MODIFIERS',
+                'GLOBAL',
+                'UNIQUE',
+                'TWO_WAY_BINDING',
+                'OTHER_DIRECTIVES',
+                'OTHER_ATTR',
+                'EVENTS',
+                'CONTENT',
+            ],
+            'alphabetical': false,
+        }],
         'vue/no-v-html': 'off',
         'vue/order-in-components': 'error',
         'vue/this-in-template': 'error',
